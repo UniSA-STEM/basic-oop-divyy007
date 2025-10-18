c5de3e2415d01d4751b5c99e5c712cbe61c8f908
divyy007 <divyy007@mymail.unisa.edu.au>
2025-10-19 02:33:25 +1030

    Started assignment with basic details

--- Rig.py
+++ Rig.py
@@ -1,8 +1,8 @@
 """
 File: Rig.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: <Divyesh>
+ID: <110449639>
+Username: <divyy007>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
