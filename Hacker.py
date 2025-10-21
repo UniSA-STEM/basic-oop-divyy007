858db6f8e5421e95732c5ef8e61c3dcb4fd05e62
divyy007 <divyy007@mymail.unisa.edu.au>
2025-10-21 21:32:52 +1030

    created class Hacker and encapsulated the attributes

--- Hacker.py
+++ Hacker.py
@@ -2,10 +2,16 @@
 File: Hacker.py
 Description: <Represents hacker>
 Author: <Divyesh>
-ID: <110445639>
+ID: <110449639>
 Username: <divyy007>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
-class Hacker:
+from rig import Rig
+from asset import Asset
 
-    pass
+class Hacker:
+    def init(self, name):
+        self.__name = name
+        self.__trace_level = 0
+        self.__rig = None          # hacker can have one rig at a time
+        self.__inventory = [Asset("CryptoToken", "Used to get a rig")]  # currency in inventory
