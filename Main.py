11224e51f4fe4d68e4605338b297adee77f7fe01
divyy007 <divyy007@mymail.unisa.edu.au>
2025-10-21 23:55:25 +1030

    tested upgrade, launch attack, rig condition and repair

--- Main.py
+++ Main.py
@@ -32,3 +32,21 @@ hacker1.get_inventory().append(Asset("Hardware Patch", "Upgrade rigs."))
 hacker1.get_inventory().append(Asset("Security Chip", "To encrypt or decrypt assets."))
 hacker1.get_inventory().append(Asset("CryptoToken", "For repair."))
 hacker2.get_inventory().append(Asset("Security Chip", "To encrypt or decrypt assets."))
+
+# Step 3: Upgrade Hacker1’s Rig
+print("\nStep 3: Upgrading Rig")
+hacker1.upgrade_rig()
+
+# Step 4: Launch attacks from Hacker1 to Hacker2’s Rig
+print("\nStep 4:Launch Attacks")
+hacker1.launch_attack(hacker2)
+hacker1.launch_attack(hacker2)
+
+# Step 5: Check rig condition
+print("\nStep 5: Rig Conditions")
+print(hacker1.get_rig())
+print(hacker2.get_rig())
+
+# Step 6: Repair a broken rig using CryptoToken
+print("\nStep 6: Repairing Rig")
+hacker1.get_rig().repair()
